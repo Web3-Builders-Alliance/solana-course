1f2e9bcab0e1605cbe4ceb95625e383968263863
aresastro <dean@bitping.com>
2023-05-05 18:32:26 +0800

    Trying this path

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  basePath: '/solana-course',
-  assetPrefix: isProd ? '/solana-course' : '',
+  basePath: isProd ? '/solana-course' : '',
+  assetPrefix: isProd ? '/solana-course/' : '',
   images: {
     unoptimized: true,
   },
