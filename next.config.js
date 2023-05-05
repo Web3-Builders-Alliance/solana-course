2f20eed81d27f29456e07dcff73c848864c99ff5
aresastro <dean@bitping.com>
2023-05-05 17:52:15 +0800

    Fixed image directory

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 // // module.exports = {
 // //     reactStrictMode: true,
 // // };
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/Web3-Builders-Alliance/solana-course/' : '',
   images: {
     unoptimized: true,
-  },
+  }
 }
