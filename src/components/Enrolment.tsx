8d01158da713e129a78bdeb15bb845ad0ef47b73
aresastro <dean@bitping.com>
2023-05-06 01:20:38 +0800

    cleanup

--- src/components/Enrolment.tsx
+++ src/components/Enrolment.tsx
@@ -22,51 +22,77 @@ const ExampleComponent: React.FC<Props> = ({ publicKey }) => {
     const [accountData, setAccountData] = useState<AccountData>({ owner: "", address: "", github: "", avatar: "", repo: ""});
     const [loading, setLoading] = useState<boolean>(false);
     useEffect(() => {
-        setLoading(true);
-        const fetchAccountData = async () => {
-            const pubkey = new PublicKey(publicKey!)
-            const programId = new PublicKey('HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1')
-            const prereq = new TextEncoder().encode('prereq')
-            const [ pda, _bump ] = PublicKey.findProgramAddressSync([prereq, pubkey.toBytes()], programId)
-            const address = pda.toBase58();
-            const { data } = await axios.post('https://api.devnet.solana.com', {
-                "jsonrpc": "2.0",
-                "id": 1,
-                "method": "getAccountInfo",
-                "params": [
+        const fetchGithubUser = async (username: string) => {
+            let github = '';
+            let avatar = '';
+            try {
+                const { data } = await axios.get(`https://api.github.com/users/${username}`);
+                github = data.login;
+                avatar = data.avatar_url;
+            } catch(e) {
+                console.error(e);
+            }
+            return { github, avatar }
+        }
+
+        const fetchGithubRepo = async (username: string) => {
+            let repo = '';
+            try {
+                const { data } = await axios.get(`https://api.github.com/repos/Web3-Builders-Alliance/${username}-Solana-Q2-2023`)
+                repo = data.name
+            } catch(e) {
+                console.error(e)
+            }
+            return { repo }
+        }
+
+        const loadEnrolment = async (publicKey: string) => {
+            let github = '';
+            let owner = '';
+            let address = '';
+            try {
+                const pubkey = new PublicKey(publicKey!)
+                const programId = new PublicKey('HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1')
+                const prereq = new TextEncoder().encode('prereq')
+                const [ pda, _bump ] = PublicKey.findProgramAddressSync([prereq, pubkey.toBytes()], programId)
+                address = pda.toBase58();
+                const { data } = await axios.post('https://api.devnet.solana.com', {
+                    "jsonrpc": "2.0",
+                    "id": 1,
+                    "method": "getAccountInfo",
+                    "params": [
+                    address,
+                        {
+                            "encoding": "jsonParsed"
+                        }
+                    ]
+                });
+                const bytes = data.result?.value?.data[0] ? Buffer.from(data.result?.value?.data[0], 'base64') : null;
+                if (!bytes) { 
+                    address = '';                   
+                    throw new Error("Data not found")
+                }
+                github = bytes.subarray(12,51).toString("utf-8").replace("\0", "")
+                owner = new PublicKey(bytes.subarray(51, 83)).toBase58()
+            } catch (e) {
+                console.error(e)
+            }
+            return {
                 address,
-                    {
-                        "encoding": "jsonParsed"
-                    }
-                ]
-            });
-            console.log(data);
-            const bytes = Buffer.from(data.result?.value?.data[0], 'base64');
-            const github_string = bytes.subarray(12,51).toString("utf-8").replace("\0", "");
-            const owner = new PublicKey(bytes.subarray(51, 83)).toBase58();
-            setAccountData({
-                ...accountData,
-                owner,
-                address,
-                github: github_string
-            })
-            const { data: github_user } = await axios.get(`https://api.github.com/users/${github_string}`);
-            const github = github_user.login || null;
-            const avatar = github_user.avatar_url || null;
-            setAccountData({
-                ...accountData,
                 github,
-                avatar
-            })
-            const { data: github_repo } = await axios.get(`https://api.github.com/repos/Web3-Builders-Alliance/${github}-Solana-Q2-2023`)
-            const repo = github_repo.name || null
-            setAccountData({
-                ...accountData,
-                repo
-            })
+                owner                
+            }
+        }
+
+        const fetchAccountData = async (publicKey: string) => {
+            let acc: AccountData = { ...accountData, ...(await loadEnrolment(publicKey)) };
+            acc = { ...acc, ...(await fetchGithubUser(acc.github)) };
+            acc = { ...acc, ...(await fetchGithubRepo(acc.github)) };            
+            setAccountData(acc)
             setLoading(false);
         }
-        fetchAccountData().catch((e) => {
+        setLoading(true)
+        fetchAccountData(publicKey!).catch((e) => {
             setLoading(false)
             console.error(e)
         });
@@ -82,10 +108,13 @@ const ExampleComponent: React.FC<Props> = ({ publicKey }) => {
                 {
                     accountData.avatar && accountData.github ? <img alt={accountData.github} className="w-20 rounded-md mx-auto mb-10 border border-slate-600" src={accountData.avatar} /> : null
                 }
-                <Tick>Account created</Tick>
-                <Tick>Owner match</Tick>
                 {
-                    accountData.avatar ? <Tick>Github:<code className="text-sm ml-2 font-bold bg-slate-700 px-2 py-1">{ accountData.github }</code></Tick> : <Cross>Github user <code className="text-sm ml-2 font-bold bg-slate-700 px-2 py-1">{ accountData.github }</code> not found</Cross>
+                    accountData.address ? <Tick>Account created</Tick> : <Cross>Account not found</Cross>
+                }
+                {
+                    accountData.avatar ? 
+                    <Tick>Github:<code className="text-sm ml-2 font-bold bg-slate-700 px-2 py-1">{ accountData.github }</code></Tick> : 
+                    <Cross>Github user {accountData.github ? <code className="text-sm ml-2 font-bold bg-slate-700 px-2 py-1">{ accountData.github }</code> : null } not found</Cross>
                 }
                 {
                     accountData.repo ? <Tick>Repo: {accountData.repo}</Tick> : <Cross>Git repo not configured yet</Cross>
